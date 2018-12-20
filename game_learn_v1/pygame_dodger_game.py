0edab3c698b84f50d33b2bf9a1158777bf3c2411
superniaoren <superniaoren@yahoo.com>
2018-12-20 23:52:03 +0800

    follow the book, add the full dodger code

--- game_learn_v1/pygame_dodger_game.py
+++ game_learn_v1/pygame_dodger_game.py
@@ -72,7 +72,7 @@ topScore = 0
 while True:
     # set up the start 
     baddies = []
-    score = []
+    score = 0
     playerRect.topleft = (WindowWidth / 2, WindowHeight - 50)
     moveLeft = moveRight = moveUp = moveDown = False
     reverseCheat = slowCheat = False
@@ -90,3 +90,95 @@ while True:
                     reverseCheat = True 
                 elif event.key == pg.K_x:
                     slowCheat = True 
+                elif event.key == pg.K_LEFT or event.key == pg.K_a:
+                    moveLeft = True
+                    moveRight = False
+                elif event.key == pg.K_RIGHT or event.key == pg.K_d:
+                    moveLeft = False
+                    moveRight = True
+                elif event.key == pg.K_UP or event.key == pg.K_w:
+                    moveUp = True
+                    moveDown = False
+                elif event.key == pg.K_DOWN or event.key == pg.K_s:
+                    moveUp = False
+                    moveDown = True
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_z:
+                    reverseCheat = False
+                    score = 0
+                elif event.key == pg.K_x:
+                    slowCheat = False
+                    score = 0
+                elif event.key == pg.K_ESCAPE:
+                    terminate() 
+                elif event.key == pg.K_LEFT or event.key == pg.K_a:
+                    moveLeft = False
+                elif event.key == pg.K_RIGHT or event.key == pg.K_d:
+                    moveRight = False
+                elif event.key == pg.K_UP or event.key == pg.K_w:
+                    moveUp = False
+                elif event.key == pg.K_DOWN or event.key == pg.K_s:
+                    moveDown = False
+            elif event.type == pg.MOUSEMOTION:
+                # move the player to the cursor
+                playerRect.centerx = event.pos[0]
+                playerRect.centery = event.pos[1]
+            # add new baddies 
+            if not reverseCheat and not slowCheat:
+                baddieAddCounter += 1
+            if baddieAddCounter == addNewBaddieRate:
+                baddieAddCounter = 0
+                baddieSize = random.randint(baddieMinSize, baddieMaxSize)
+                newBaddie = {'rect': pg.Rect(random.randint(0, WindowWidth - baddieSize), 0 - baddieSize, baddieSize, baddieSize), 
+                             'speed': random.randint(baddieMinSpeed, baddieMaxSpeed),
+                             'surface': pg.transform.scale(baddieImage, (baddieSize, baddieSize)), }
+                baddies.append(newBaddie)
+        # move the player
+        if moveLeft and playerRect.left > 0:
+            playerRect.move_ip(-1 * playerMoveRate, 0)
+        elif moveRight and playerRect.right < WindowWidth:
+            playerRect.move_ip(+1 * playerMoveRate, 0)
+        elif moveUp and playerRect.top > 0:
+            playerRect.move_ip(0, -1 * playerMoveRate)
+        elif moveDown and playerRect.bottom < WindowHeight:
+            playerRect.move_ip(0, +1 * playerMoveRate)
+        # move the baddie down
+        for bd in baddies:
+            if not reverseCheat and not slowCheat:
+                bd['rect'].move_ip(0, bd['speed'])
+            elif reverseCheat:
+                bd['rect'].move_ip(0, -5)
+            elif slowCheat:
+                bd['rect'].move_ip(0, 1)
+        # delete fallen baddie
+        for bd in baddies[:]:
+            if bd['rect'].top > WindowHeight:
+                baddies.remove(bd)
+        # draw the window
+        windowSurface.fill(backgroundColor)
+        # draw the score + top score
+        drawText('score: %s' % (score), font, windowSurface, 10, 0)
+        drawText('top score: %s' % (topScore), font, windowSurface, 10, 40)
+        # draw the player
+        windowSurface.blit(playerImage, playerRect)
+        # draw baddie
+        for bd in baddies:
+            windowSurface.blit(bd['surface'], bd['rect'])
+
+        pg.display.update()
+        # collide check
+        if playerHitBaddie(playerRect, baddies):
+            if score > topScore:
+                topScore = score
+            break
+        mainClock.tick(fps)
+
+    # stop game
+    pg.mixer.music.stop()
+    gameoverSound.play()
+    drawText('game over', font, windowSurface, (WindowWidth / 3), (WindowHeight / 3))
+    drawText('press a key to play again', font, windowSurface, WindowWidth/3-80, WindowHeight/3+50)
+    pg.display.update()
+    waitForPlayerToPressKey()
+
+    gameoverSound.stop()
