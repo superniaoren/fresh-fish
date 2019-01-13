8bef7ac7953cdaabeab7c3a23ffac9d7d15936a2
superniaoren <superniaoren@yahoo.com>
2019-01-13 22:01:52 +0800

    add io syscall test

--- effective_python_learn_v1/test_subprocess_pseudo_parallel.py
+++ effective_python_learn_v1/test_subprocess_pseudo_parallel.py
@@ -1,7 +1,12 @@
 import os, sys, time, subprocess
 from threading import Thread
 
+import select
+
 ## sequential computation, async/parallel I/O operation.
+
+def pseudo_io():
+    select.select([], [], [], 0.1)  # 0.1s
 
 def factorize(num):
     for i in range(1, num + 1):
@@ -44,3 +49,24 @@ if __name__ == '__main__':
         thread.join()
     end = time.time()
     print("threading: time elapsed %.3f seconds" % (end - start))
+
+    print("(pseudo) blocking IO mode:")
+    start = time.time()
+    for _ in range(3):
+        pseudo_io()
+    end = time.time()
+    print("pseudo io: time elapsed %.3f seconds" % (end - start))
+
+    print("(async) blocking IO mode:")
+    start = time.time()
+    threads = []
+    for _ in range(3):
+        thread = Thread(target=pseudo_io)
+        #threads.append(thread)
+        thread.start()
+        threads.append(thread)
+        ## TODO: the computtion logic
+    for thread in threads:
+            thread.join()
+    end = time.time()
+    print("async io: time elapsed %.3f seconds" % (end - start))
