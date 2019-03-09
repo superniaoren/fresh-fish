83aea8f50d98cb773b0d3b746d3957552f103a85
superniaoren <superniaoren@yahoo.com>
2019-03-09 23:27:21 +0800

    add coroutine example: compute min

--- effective_python_learn_v1/1_parallel_concurrent/test_coroutine.py
+++ effective_python_learn_v1/1_parallel_concurrent/test_coroutine.py
@@ -1,6 +1,19 @@
 ##
 import os, sys
 
+def my_coroutine():
+    while True:
+        recieve = yield
+        print('received: ', recieve)
+
+def minimize_coroute():
+    # taken as the 1st input & minimize value
+    minimize = yield
+    while True:
+        input_value = yield minimize
+        minimize = min(input_value, minimize)
+    # although U wanna return a value, the type is still 'generator'
+    #return minimize
 
 if __name__ == '__main__':
     # iterators: store all the values in memory; not memory-efficient
@@ -14,4 +27,19 @@ if __name__ == '__main__':
 
     # generators: NOTE the difference between tuple and generator !!
     # a = ('cc', 23) --> tuple;  b = (x * x for x in range(10)) -> generator
+    # Generators are iterators, a kind of iterable you can only iterate over once. 
+    # Generators do not store all the values in memory, they generate the values on the fly:
     
+    it = my_coroutine()
+    print('type: ', type(it))
+    next(it)
+    it.send('1st')
+    it.send('2nd')
+
+    mini_it = minimize_coroute()
+    print('type: ', type(mini_it))
+    next(mini_it)
+    print(mini_it.send(10))
+    print(mini_it.send(30))
+    print(mini_it.send(5))
+    print(mini_it.send(1))
