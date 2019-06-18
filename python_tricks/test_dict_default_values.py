78e29890dd11f137707558b0daa2f6483c14b952
superniaoren <superniaoren@yahoo.com>
2019-06-19 07:51:58 +0800

    update dict default test

--- python_tricks/test_dict_default_values.py
+++ python_tricks/test_dict_default_values.py
@@ -6,6 +6,7 @@ name_uid = {
     373: 'PB',
 }
 
+# dict query
 def greeting(uid):
     # v0
     #return 'Alexander Yogoivanivch %s!' % name_uid[uid]
@@ -22,7 +23,34 @@ def greeting(uid):
     # v3, dict's get() method:
     return 'Alexander Yogoivanivch %s!' % name_uid.get(uid, 'Hi, there, and who\'re U:{}'.format(uid))
 
+def customize_sort(input_dict):
+    print('type of input: {}'.format(type(input_dict)))
+    print('type of input.items: {}'.format(type(input_dict.items())))
+    #default sort, with default keys
+    default = sorted(input_dict.items())
+    print(default)
+    print('type of output: {}'.format(type(default)))
+
+    # sort on values (not keys)
+    #vsort = sorted(input_dict.items(), key=lambda x: x[0]) 
+    vsort = sorted(input_dict.items(), key=lambda x: x[1]) 
+    print(vsort)
+    print('type of output: {}'.format(type(vsort)))
+
+    # use operator module to replace the lambda-based index lookup
+    import operator
+    osort = sorted(input_dict.items(), key=operator.itemgetter(1))
+    print(osort)
+    print('type of output: {}'.format(type(osort)))
+
+    # abs sort, NOTE: reverse = False by default
+    asort = sorted(input_dict.items(), key=lambda x: abs(x[1]), reverse=True)
+    print(asort)
+    print('type of output: {}'.format(type(asort)))
 
 if __name__ == '__main__':
     print(greeting(19.0))
     print(greeting(41))
+
+    idict = {'a': 92, 'xs': 3, 'zoo':-4, 'yolo': 13}
+    customize_sort(idict)
