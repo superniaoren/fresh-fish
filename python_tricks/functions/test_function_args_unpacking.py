2400990bf4c090e464049bc391cbcd3765d23ed2
superniaoren <superniaoren@yahoo.com>
2019-07-30 08:07:05 +0800

    match args

--- python_tricks/functions/test_function_args_unpacking.py
+++ python_tricks/functions/test_function_args_unpacking.py
@@ -13,3 +13,14 @@ if __name__ == '__main__':
         dump_vector(**dict_args)
     except:
         print("the args not match up!!!!")
+
+    dict_match = {'x': 'enormous', 'y': 'stress', 'z': -0.5, 'w': 23}
+    try:
+        dump_vector(*dict_match)
+    except:
+        print("the args not match up!!!!")
+    
+    try:
+        dump_vector(**dict_match)
+    except:
+        print("the args not match up!!!!")
