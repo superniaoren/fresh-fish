122525e1b1647bde8144e022f212e52af46e6804
superniaoren <superniaoren@yahoo.com>
2019-07-30 08:31:10 +0800

    update function return

--- python_tricks/functions/test_function_return.py
+++ python_tricks/functions/test_function_return.py
@@ -19,6 +19,10 @@ def pretend_default(value):
     #return 
 
 if __name__ == '__main__':
+    print(type(pretend(0)))
+    print(type(pretend_none(0)))
+    print(type(pretend_default(0)))
+    print('-' * 40)
     print(pretend(0))
     print(pretend_none(0))
     print(pretend_default(0))
