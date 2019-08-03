6a286c830a0fafb6dede20867297b8119bed6a8b
superniaoren <superniaoren@yahoo.com>
2019-08-03 08:13:15 +0800

    update string str/repr conversion

--- python_tricks/classes_oop/test_oop_string_conversion.py
+++ python_tricks/classes_oop/test_oop_string_conversion.py
@@ -17,6 +17,21 @@ class WithStr:
 
     def __str__(self):
         return f'IQIYI: {self.band_native}, my favorite.'
+
+class WithStrRepr:
+    def __init__(self, 
+                 beatles : str, 
+                 click15 : str):
+        self.band_britan = beatles
+        self.band_native = click15
+    
+    def __str__(self):
+        return f'__str__:: IQIYI: {self.band_native}, my favorite.'
+
+    def __repr__(self):
+        return f'__repr__:: YOUKU: {self.band_native}, my favorite.'
+        
+
     
 
 if __name__ == '__main__':
@@ -26,7 +41,24 @@ if __name__ == '__main__':
     # (which is the object's memory address in CPython)
     nonrepr = NonRepr('jude', 'beidaihe')
     print(nonrepr)
+    print('-' * 40)
 
     # with __str__
     withstr = WithStr('nov', 'penglei')
     print(withstr)
+    print('-' * 40)
+
+    # with __str__ and __repr__
+    withstrrepr = WithStrRepr('stark', 'pangkuan')
+    strrepr_list = [withstrrepr]
+    print(withstrrepr)
+    print(str(withstrrepr))
+    # funny, containers like lists and dicts always use the results of __repr__ 
+    # to represnet the objects they contain. Even if U call str on the container
+    print(str(strrepr_list))  
+    print('-' * 40)
+
+    # use built-in function
+    print(repr(withstrrepr))
+    print(repr(strrepr_list))  
+    print('-' * 40)
