7f9bb2c150b2d96bb82b16f2e70f13dc25c417a1
superniaoren <superniaoren@yahoo.com>
2019-08-12 07:55:37 +0800

    update own exception

--- python_tricks/classes_oop/test_oop_own_exception.py
+++ python_tricks/classes_oop/test_oop_own_exception.py
@@ -5,6 +5,17 @@ def hiphop(singer_name: str):
     if len(singer_name) < 11:
         raise ValueError
 
+# self-documenting
+class DIYTooShortError(ValueError):
+    pass
+
+def validateTooShort(singer_name: str):
+    print(singer_name)
+    if len(singer_name) < 11:
+        raise DIYTooShortError(singer_name)
+
+
 if __name__ == '__main__':
     name = 'HotDog'
-    hiphop(name)
+    #hiphop(name)
+    validateTooShort(name)
