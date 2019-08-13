dbad9b93305e16bfba0c8f624dd117f71a0c6c3d
superniaoren <superniaoren@yahoo.com>
2019-08-13 08:16:00 +0800

    update custom exception

--- python_tricks/classes_oop/test_oop_own_exception.py
+++ python_tricks/classes_oop/test_oop_own_exception.py
@@ -13,6 +13,18 @@ def validateTooShort(singer_name: str):
     print(singer_name)
     if len(singer_name) < 11:
         raise DIYTooShortError(singer_name)
+    elif len(singer_name) > 20:
+        raise ValueError
+
+# create a custom exception hierarchy
+class BaseExceptionError(ValueError):
+    pass
+
+class NameTooShortError(BaseExceptionError):
+    pass
+
+class NameTooLongError(BaseExceptionError):
+    pass
 
 
 if __name__ == '__main__':
