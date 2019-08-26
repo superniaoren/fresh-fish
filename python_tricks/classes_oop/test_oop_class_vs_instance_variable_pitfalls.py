fd4c8277646b8b4bafb65a946cdd5cfa50d7516b
superniaoren <superniaoren@yahoo.com>
2019-08-26 08:02:00 +0800

    update class vs instance variable pitfalls'

--- python_tricks/classes_oop/test_oop_class_vs_instance_variable_pitfalls.py
+++ python_tricks/classes_oop/test_oop_class_vs_instance_variable_pitfalls.py
@@ -7,14 +7,80 @@
 
 class JiGong:
     hobbies = 'buddha'
+    counter= 0
 
     def __init__(self, name):
         self.who = name
+        self.__class__.counter += 1
+
+class Counter:
+    num_counter = 0
+    
+    def __init__(self, value):
+        self.num_counter += 20
+        #self.__class__.num_counter += value
+
+class BuggyCounter:
+    num_counter = 0 
+    def __init__(self):
+        self.num_counter += 1
 
 if __name__ == '__main__':
+    # init
     luck = JiGong('xiuyuan')
     taiz = JiGong('daoji')
 
     print('who: ', luck.who, taiz.who)
-
     print('hobbies: ', luck.hobbies, taiz.hobbies)
+
+    # modify the class variable
+    # here create an instance variable with the same name as the class variable
+    # NOTE: this is error-prone and is the key difference, do care about it. 
+    luck.hobbies = 'pogada'
+    taiz.hobbies = 'talkie'
+    print('hobbies: ', luck.hobbies, taiz.hobbies)
+
+    # Reason: 'trying to modify a class variable through an object instance-which then 
+    #         accidently creates an instance variable of the same name, shadowing the 
+    #         original class variable-is a bit of an OOP pitfall in Python'
+    # fetch the class var
+    print('type of inst var: ', type(luck.hobbies))
+    print('type of inst var: ', type(taiz.hobbies))
+    print('class hobbies: ', luck.__class__.hobbies, taiz.__class__.hobbies)
+    
+    # class var 
+    print('another class: ', JiGong.hobbies)
+
+    # class counter var
+    print('JiGong class: ')
+    print(JiGong('').counter)
+    print(JiGong('').counter)
+    print(JiGong.counter)
+    print(JiGong('').counter)
+    print(JiGong('').counter)
+
+    # bug 1
+    print('buggy counter 1: ')
+    print(Counter(0).num_counter)  # self.num_counter, an copy
+    print(Counter.num_counter)  # class.num_counter
+    print(Counter.num_counter)
+    counter_1 = Counter(1)
+    print(counter_1.num_counter, counter_1.__class__.num_counter)
+    counter_2 = Counter(1)
+    print(counter_2.num_counter, counter_2.__class__.num_counter)
+    counter_3 = Counter(1)
+    print(counter_3.num_counter, counter_3.__class__.num_counter)
+    counter_4 = Counter(1)
+    print(counter_4.num_counter, counter_4.__class__.num_counter)
+
+    # bug 2
+    print('buggy counte 2: ')
+    print(BuggyCounter.num_counter)
+    print(BuggyCounter().num_counter)# here return the instance 'num_counter'
+    print(BuggyCounter.num_counter)  # class num_counter not increased
+    print(BuggyCounter().num_counter)
+    print(BuggyCounter.num_counter)
+    print(BuggyCounter().num_counter)
+
+
+
