b6a28b97cf6db922290c96c875546bf5d00510ec
superniaoren <superniaoren@yahoo.com>
2019-08-27 08:20:41 +0800

    add instance and class and static methods

---
+++ python_tricks/classes_oop/test_oop_instance_class_static_methods.py
@@ -0,0 +1,4 @@
+# 1, 'instance method need a class instance and can access the instance through self'
+# 2, class methods can't access the instance(self) but they have access to the class 
+#    itself via cls.
+# 3, 
