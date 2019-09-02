e355523ab07549fa75acb1af0f4f5747994c9257
superniaoren <superniaoren@yahoo.com>
2019-09-02 08:27:41 +0800

    update instance+class+static methods

--- python_tricks/classes_oop/test_oop_instance_class_static_methods.py
+++ python_tricks/classes_oop/test_oop_instance_class_static_methods.py
@@ -25,6 +25,20 @@ class EFClass:
     def staticmethod():
         return 'static method '
 
+class Pizza:
+    def __init__(self, foods):
+        self.foods = foods
+    def __repr__(self):
+        return f'Pizza ({self.foods!r})'
+
+    @classmethod
+    def pizza_1(cls):
+        return cls(['yang', 'niu'])
+
+    @classmethod
+    def pizza_2(cls):
+        return cls(['luke', 'nound', 'shawn'])
+
 if __name__ == '__main__':
     efc = EFClass()
     # instance method
@@ -42,4 +56,21 @@ if __name__ == '__main__':
     print('efc static: ', EFClass.staticmethod())
     #print('efc static: ', EFClass.staticmethod(efc))
 
-    # 
+    # static method: 
+    # can't access class or instance state because they don't take a 'cls' or 'self' argument.
+    # flagging a method as a static method is not just a hint that a method won't modify class 
+    #    or instance state. This restriction is also enforced by the Python runtime.
+    # In practice, it often helps avoid accidental modification that go against the original design.
+
+    # static & class methods are ways to communicate developer intent while enforcing that intent
+    #    enough to avoid most 'slip of the mind' mistakes and bugs that would break the design
+
+    # class: factory methods ? 
+    # Here defind alternative constructors for your classes
+    # Python only allows one __init__ method per class. Using class methods makes it possible to add
+    # as many alternative constructors as neccessary.
+    print(Pizza.pizza_1())
+    print(Pizza.pizza_2())
+
+
+
