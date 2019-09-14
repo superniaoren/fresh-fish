5f5d82d12583efd1e992047abde261dbbeeb468e
superniaoren <superniaoren@yahoo.com>
2019-09-14 22:56:44 +0800

    update dictionary map hashtables

--- python_tricks/common_data_structure/test_dictionaries_maps_hashtables.py
+++ python_tricks/common_data_structure/test_dictionaries_maps_hashtables.py
@@ -43,5 +43,38 @@ if __name__ == '__main__':
     dd = defaultdict(list)
     print("default dict: ", dd)
     print("default dict val: ", dd['zoo'])
+    # accessing a missing 'key' creates it and initializes it using 
+    # the default factory, 
+    dd['meng'].append('R')
+    dd['meng'].append('U')
+    dd['meng'].append('S')
+    dd['meng'].append('T')
+    print('dd[\'meng\']: ', dd['meng'])
 
+    # the data structure groups multiple dictionaries into a single mapping.
+    # lookups search the underlying mappings one by one until a key is found.
+    # insertions, updates, and deletions only affect the first mapping added to the chain.
+    from collections import ChainMap
+    dict1 = {'one': 1, 'two': 2}
+    dict2 = {'two': 3, 'three': 3}
+    dict3 = {'two': 2, 'four': 4}
+    chain = ChainMap(dict1, dict2, dict3)
+    print('ChainMap: ', chain)
+    print('chain[\'two\'] = ', chain['two'])
+    del(chain['two'])
+    print('dict1: ', dict1)
+    print('dict2: ', dict2)
+    print('dict3: ', dict3)
+    print('chain[\'two\'] = ', chain['two'])
+    #print('chain[\'missing\'] = ', chain['missing'])
 
+    # It is a wrapper around a standard dictionary that provides a read-only view 
+    # into the wrapper dictionary's data.
+    from types import MappingProxyType
+    writable = {'one': 1, 'two': 2}
+    read_only = MappingProxyType(writable)
+    print('read_only: one = ', read_only['one'])
+    read_only['one'] = 3.18
+    writable['one'] = 3.19
+    print('read_only: one = ', read_only['one'])
+    print('mappingproxy: read_only: ', read_only)
