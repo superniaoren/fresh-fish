e943211e83d3413dd50994036887943d0bc1c54a
superniaoren <superniaoren@yahoo.com>
2019-09-19 08:07:09 +0800

    update array data structure

--- python_tricks/common_data_structure/test_array_data_structure.py
+++ python_tricks/common_data_structure/test_array_data_structure.py
@@ -1,6 +1,46 @@
-
-
+import array
+# 1,[mutable/immutable]: item assignment, item deletion
+# 2,hold arbitrary data types
 if __name__ == '__main__':
-    # standard array: the list are mutable
+    # standard array: the list are mutable, and can hold arbitrary data types
+    print('-' * 20, " list, mutable dynmaic arrays ")
     arr = ['gold', 'pogoda', 'abudo']
     print(arr)
+    del(arr[1])
+    print(arr)
+    arr.append(3.14115)
+    print(arr)
+    
+    print('-' * 20, " tuple, immutable containers ")
+    #tpl = ('bagger', 'vanish', 'puff')
+    tpl = 'bagger', 'vanish', 'puff'
+    print(tpl)
+    #tpl[1] = 'release'  # immutable, not support item assignment
+    #del(tpl[1])   # neither support item deletion
+    # tuple concatenation !!!  (1) is type of int, not tuple
+    tpl = tpl + (33.0,)  #  can only concatenate tuple (not "float") to tuple
+    tpl = tpl + (33,)  # so, must use (int/float,), not (int/float)
+    print(tpl)
+
+    print('-' * 20, " array, basic typed arrays")
+    tarr = array.array('f', [-9.0, 8.0, -7.0, 6.0])
+    print('nice repr: ', tarr)
+    tarr[3] = -5.0  
+    print('mutable: ', tarr)
+    del(tarr[1])
+    print('mutable: ', tarr)
+    tarr.append(4.0)
+    print('mutable: ', tarr)
+    #tarr[3] = 'typed' # so must be the 'type', here should be float
+    
+    print('-' * 20, " str, immutable arrays of unicode charactors")
+    sarr = 'remind us people of night'
+    #sarr[1] = 'A'  # immutable, 
+    #del(sarr[3])   # immutable
+    larr = list(sarr)
+    print('can be unpacked into a list, to get a mutable repr: ')
+    print(larr)
+    marr = ''.join(list(sarr))
+    print('back to immutable string: ', marr)
+    #del(marr[0])
+    print("strings are recursivee data structures: ", type(sarr), type(sarr[0]))
