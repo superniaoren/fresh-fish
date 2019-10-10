d72e6e90ff06c6bbee9943c2fc207d8e79c6f993
superniaoren <superniaoren@yahoo.com>
2019-10-10 08:30:51 +0800

    update test records_structs_transfer_objects

--- python_tricks/common_data_structure/test_records_structs_transfer_objects.py
+++ python_tricks/common_data_structure/test_records_structs_transfer_objects.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     ## there's little protection against misspelled field names.
     ## these properties can introduce surprising bugs.
     ## be a trade-off to be made between convenience and error resilience.
-    print('-' * 40, " simple dict ")
+    print('\n', '-' * 40, " simple dict ")
     gods = {
         'cui': 'marriage',
         'cox': True,
@@ -15,14 +15,47 @@ if __name__ == '__main__':
     print('simple data object: [dict]: ', gods)
 
     
-    print('-' * 40, " immutable tuple ")
+    print('\n', '-' * 40, " immutable tuple ")
     print('\n\n tuple cost less memory than list: ')
     import dis
     print('\n\n constructing a tuple constant SHOULD take a single LOAD_CONST opcode:')
-    dis.dis(compile("('mood', 2.1414, true, false)", '', 'eval'))
+    dis.dis(compile("('mood', 2.1414, True, False)", '', 'eval'))
     dis.dis(compile("('mood', 2.1414, 'true', 'false')", '', 'eval'))
     print('\n\n constructing a list object SHOULD take several LOAD_CONST opcode:')
-    dis.dis(compile("['mood', 2.1414, true, false]", '', 'eval'))
+    dis.dis(compile("['mood', 2.1414, True, False]", '', 'eval'))
     dis.dis(compile("['mood', 2.1414, 'true', 'false']", '', 'eval'))
 
-    
+    print('a downside: tuple can only be pulled out by accessing it through integer indexes:')
+    print('thus user may mixing up the filed order .')
+    plain_tpls = ('mood', 2.1414, True, False)
+    print('[1]:', plain_tpls[1])
+
+
+    print('\n', '-' * 40, " custom class ")
+    print('more work, more control. use regular python classes as record data types.')
+    print('the default string representation for objects instantiated from custom classes is not helpful.')
+    print('To fix that, you may have to add your own __repr__ method.')
+    class DIY:
+        def __init__(self, color, weight, skin):
+            self.color = color
+            self.weight = weight
+            self.skin = skin
+
+        def __repr__(self):
+            return (f'color({self.color!r}),'
+                   f'weight({self.weight!a}),'
+                   f'skin({self.skin}),')
+
+    diy1 = DIY('purple', 291., 'Black')
+    diy2 = DIY('orange', -12., 'Brown')
+    print(diy1)
+    print(diy2)
+    # classes are mutable, if wanna add more access control and to create read-only fields using @property decorator
+    diy1.color = 'grey'
+    diy2.skin = 'unknown'
+    print(diy1)
+    print(diy2)
+
+
+
+    print('\n', '-' * 40, " custom class ")
