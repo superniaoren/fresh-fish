71280c43b8d1e84d8f647e14116826b4cf9c570b
superniaoren <superniaoren@yahoo.com>
2019-11-10 12:25:07 +0800

    python loop usage, add 4 examples

--- python_tricks/looping_and_iteration/test_pythonic_loops.py
+++ python_tricks/looping_and_iteration/test_pythonic_loops.py
@@ -24,3 +24,19 @@ if __name__ == '__main__':
     print('2nd enumerate: ')
     for i, item in enumerate(ef_items):
         print(f'{i}: {item}')
+
+    # iterators in Python can return more than just one value. They can return tuples
+    # with an arbitrary number of values that can then be unpacked right inside the for-statement.
+    print('3rd, iterate items: ')
+    linear_algebra = {
+        'vec2d': 'x,y',
+        'matrix': 'tensor',
+        'basic': 'elements',
+    }
+    for item, content in linear_algebra.items():
+        print(f'{item} -> {content}')
+
+    #range() with start value, stop value and step size
+    print('4th, stepped for loop: ')
+    for i in range(0, 20, 4):
+        print(f'stepped: {i}') 
