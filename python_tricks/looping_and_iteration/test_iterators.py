9b8cdc655aae796ff9d1fe19d4ba434187f19cb7
superniaoren <superniaoren@yahoo.com>
2019-11-17 22:25:51 +0800

    start to test iterator, not finished

---
+++ python_tricks/looping_and_iteration/test_iterators.py
@@ -0,0 +1,28 @@
+## known as 'beautiful iterators'
+##
+
+class Repeater:
+    def __init__(self, value):
+        self.value = value
+    
+    def __iter__(self):
+        return RepeaterIterator(self)
+
+class RepeaterIterator:
+    def __init__(self, source):
+        self.source = source
+        self.counter = 0
+
+    def __next__(self):
+        if self.counter < 10:
+            return self.source.value
+        else:
+            self.counter += 1
+
+if __name__ == '__main__':
+    print('learn iterators : ') 
+
+    repeater = Repeater('Tatoon')
+    for item in repeater:
+        print(item)
+
