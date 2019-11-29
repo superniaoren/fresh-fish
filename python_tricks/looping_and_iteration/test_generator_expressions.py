7d22a08529e1db29fd326f37fb07621ef21ecea2
superniaoren <superniaoren@yahoo.com>
2019-11-29 23:56:48 +0800

    update generator-expression

--- python_tricks/looping_and_iteration/test_generator_expressions.py
+++ python_tricks/looping_and_iteration/test_generator_expressions.py
@@ -7,3 +7,23 @@ if __name__ == '__main__':
     print(next(iterator))
     print(next(iterator))
     #print(next(iterator))  # StopIteration
+
+    def bounded_repeater(value, max_counter):
+        for i in range(max_counter):
+            yield value
+
+    iterator = bounded_repeater('Sadiago', 4)
+    #print(next(iterator))
+    #print(next(iterator))
+    #print(next(iterator))
+    #print(next(iterator))
+    for x in iterator:
+        print(x)
+
+    #next(iterator)
+
+    # list comprehensions vs generator expressions
+    genexpr = ('Saint' for i in range(3))
+    listcomp = ['Boom' for i in range(3)] 
+    print('Genexpr: ', genexpr)
+    print('ListComp: ', listcomp)
